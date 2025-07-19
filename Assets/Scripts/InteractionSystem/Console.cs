367e29bb40509efcf74baf03246a871442a78348
Brian Lee <leun.se781@gmail.com>
2025-07-20 00:13:22 +0800

    door stuff

--- Assets/Scripts/InteractionSystem/Console.cs
+++ Assets/Scripts/InteractionSystem/Console.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Console : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
-
+    private bool _consoleStatus = false;
+    [SerializeField] private UnityEvent _event;
     public string InteractionPrompt => _prompt;
 
     public bool Interact(Interactor interactor)
@@ -17,8 +19,14 @@ public class Console : MonoBehaviour, IInteractable
         }
         else
         {
-            Debug.Log("Console used, opening door");
+            if(_consoleStatus == false)
+            {
+                Debug.Log("openingDoor");
+                _event.Invoke();
+                _consoleStatus = true;  
+            }
             return true;
+
         }
        
     }
