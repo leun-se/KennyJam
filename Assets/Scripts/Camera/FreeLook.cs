8d9a06bd098ff6234bfeeb44cb50ee68e1199143
Elese Marie <emzukina@gmail.com>
2025-07-19 11:59:45 -0400

    fixed camera constraints

--- Assets/Scripts/Camera/FreeLook.cs
+++ Assets/Scripts/Camera/FreeLook.cs
@@ -9,6 +9,16 @@ public class FreeLook : MonoBehaviour
 
     public bool allowMouseLook = true;
 
+    public float minYaw = -85f;
+    public float maxYaw = -25f;
+
+    public float minPitch = -30f;
+    public float maxPitch = 30f;
+
+    private float initialPitch;
+
+    private float initialYaw;
+
     private Camera cam;
 
     void Start()
@@ -16,6 +26,12 @@ public class FreeLook : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         cam = GetComponent<Camera>();
+
+        initialYaw = transform.localEulerAngles.y;
+        yRotation = initialYaw;
+
+        initialPitch = transform.localEulerAngles.x;
+        xRotation = initialPitch;
     }
 
     void Update()
@@ -24,9 +40,17 @@ public class FreeLook : MonoBehaviour
             return;
 
         Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+
         yRotation += mouseDelta.x * lookSensitivity;
         xRotation -= mouseDelta.y * lookSensitivity;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        float yawMin = initialYaw + minYaw;
+        float yawMax = initialYaw + maxYaw;
+        yRotation = Mathf.Clamp(yRotation, yawMin, yawMax);
+
+        float pitchMin = initialPitch + minPitch;
+        float pitchMax = initialPitch + maxPitch;
+        xRotation = Mathf.Clamp(xRotation, pitchMin, pitchMax);
 
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
