f008d83495cf925115e2352f2dfebb985e06c746
Brian Lee <leun.se781@gmail.com>
2025-07-19 21:20:50 +0800

    lever animations

--- Assets/Scripts/InteractionSystem/Interactor.cs
+++ Assets/Scripts/InteractionSystem/Interactor.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 
 public class Interactor : MonoBehaviour
 {
+    private MindController mindController;
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
@@ -13,15 +14,29 @@ public class Interactor : MonoBehaviour
 
     private readonly Collider[] _colliders = new Collider[3];
     [SerializeField] private int _numFound;
-    
+
     private IInteractable _interactable;
+
+    private void Awake()
+    {
+        mindController = FindFirstObjectByType<MindController>();
+    }
     private void Update()
     {
+        if (mindController != null && mindController.CurrentControlledCharacter != gameObject)
+        {
+            if (_interactionPromptUI.IsDisplayed)
+                _interactionPromptUI.Close();
+
+            return;
+        }
+
         _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
 
         if (_numFound > 0)
         {
             _interactable = _colliders[0].GetComponent<IInteractable>();
+            GameObject foundCharacter = _colliders[0].gameObject;
 
             if (_interactable != null)
             {
@@ -31,8 +46,8 @@ public class Interactor : MonoBehaviour
         }
         else
         {
-           if (_interactable != null) _interactable = null;
-           if (_interactionPromptUI.IsDisplayed) _interactionPromptUI.Close();
+            if (_interactable != null) _interactable = null;
+            if (_interactionPromptUI.IsDisplayed) _interactionPromptUI.Close();
         }
 
     }
--- Assets/Scripts/InteractionSystem/Lever.cs
+++ Assets/Scripts/InteractionSystem/Lever.cs
@@ -3,12 +3,31 @@ using UnityEngine;
 public class Lever : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
+    private bool _leverStatus;
+    private Animator _anim;
 
     public string InteractionPrompt => _prompt;
 
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
     public bool Interact(Interactor interactor)
     {
-        Debug.Log("Lever used, opening door");
-        return true;
+        if (_leverStatus == false)
+        {
+            Debug.Log(_leverStatus);
+            _anim.SetBool("LeverStatus", true);
+            _leverStatus = !_leverStatus;
+            return true;
+        }
+        else
+        {
+            Debug.Log(_leverStatus);
+            _anim.SetBool("LeverStatus", false);
+            _leverStatus = !_leverStatus;
+            return false;
+        }
     }
+   
 }
